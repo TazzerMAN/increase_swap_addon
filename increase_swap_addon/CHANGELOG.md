d30410e17562dca987e03e42dd4736fc2745ac95
TazzerMAN <TazzerMAN@users.noreply.github.com>
2023-06-11 10:57:31 +0200

    Added function to display timestamp in shell script
    This commit introduces a new function, print_date, in the shell script. The function is called before each echo statement to display the current timestamp in the format [HH:mm:ss DD/MM/YYYY]. By incorporating the date command, the script now provides a more informative output with the timestamp for better tracking and monitoring.

--- increase_swap_addon/CHANGELOG.md
+++ increase_swap_addon/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [1.1.1] - 2023-06-11
+
+### Added
+- Timestamp added to log messages.
+
 ## [1.1.0] - 2023-04-26
 
 ### Added
--- increase_swap_addon/run.sh
+++ increase_swap_addon/run.sh
@@ -14,45 +14,50 @@ remove_old_swap_file() {
     if [ "${location}" != "${SWAP_LOCATION}" ]; then
       old_swap_file="/${location}/_swap.swap"
       if [ -f "${old_swap_file}" ]; then
-        echo "Removing old swap file at ${old_swap_file}..."
+        print_date "Removing old swap file at ${old_swap_file}..."
         swapoff "${old_swap_file}"
         rm -f "${old_swap_file}"
-        echo "Old swap file removed."
+        print_date "Old swap file removed."
       fi
     fi
   done
 }
 
-echo "Starting Increase Swap add-on..."
-echo "Checking swap size at ${SWAP_LOCATION}..."
+print_date() {
+  timestamp=$(date +'%H:%M:%S %d/%m/%Y')
+  echo "[$timestamp] $1"
+}
+
+print_date "Starting Increase Swap add-on..."
+print_date "Checking swap size at ${SWAP_LOCATION}..."
 
 remove_old_swap_file
 
 if [ ! -f "${SWAP_FILE}" ]; then
-  echo "Creating new swap file of ${SWAP_SIZE}M..."
+  print_date "Creating new swap file of ${SWAP_SIZE}M..."
   fallocate -l "${SWAP_SIZE}M" "${SWAP_FILE}"
   mkswap "${SWAP_FILE}"
   chmod 0600 "${SWAP_FILE}"
   swapon "${SWAP_FILE}"
-  echo "New swap file created and enabled."
+  print_date "New swap file created and enabled."
 else
   CURRENT_SWAP_SIZE=$(($(stat -c%s "${SWAP_FILE}") / (1024 * 1024)))
   if [ "${CURRENT_SWAP_SIZE}" -ne "${SWAP_SIZE}" ]; then
-    echo "Resizing swap file from ${CURRENT_SWAP_SIZE}M to ${SWAP_SIZE}M..."
+    print_date "Resizing swap file from ${CURRENT_SWAP_SIZE}M to ${SWAP_SIZE}M..."
     swapoff "${SWAP_FILE}"
     fallocate -l "${SWAP_SIZE}M" "${SWAP_FILE}"
     mkswap "${SWAP_FILE}"
     swapon "${SWAP_FILE}"
-    echo "Swap file resized and enabled."
+    print_date "Swap file resized and enabled."
   elif [[ ! $(dmesg | grep _swap.swap) = *swap\ on* ]]; then
-    echo "Swap file exists but not enabled. Enabling swap file..."
+    print_date "Swap file exists but not enabled. Enabling swap file..."
     mkswap "${SWAP_FILE}"
     swapon "${SWAP_FILE}"
-    echo "Existing swap file enabled."
+    print_date "Existing swap file enabled."
   else
-    echo "Swap file already enabled. Refreshing swap file..."
+    print_date "Swap file already enabled. Refreshing swap file..."
     swapoff "${SWAP_FILE}"
     swapon "${SWAP_FILE}"
-    echo "Swap file refreshed and enabled."
+    print_date "Swap file refreshed and enabled."
   fi
 fi
